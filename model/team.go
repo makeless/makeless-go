43fb64fe6cfc2491be1d94abcd2decc1374a78e6
Lucas Löffel <lucas@loeffel.io>
2020-04-20 16:52:29 +0200

    add user id (owner) to teams

--- model/team.go
+++ model/team.go
@@ -6,6 +6,9 @@ type Team struct {
 	Model
 	Name *string `gorm:"not null" json:"name" binding:"required,min=4"`
 
+	UserId *uint `gorm:"not null" json:"userId"`
+	User   *User `json:"user"`
+
 	Users []*User `gorm:"many2many:user_teams;" json:"-"`
 
 	*sync.RWMutex `json:"-"`
