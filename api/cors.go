ce6b987540f944f24615686dd3b019d48d583910
Lucas Löffel <lucas@loeffel.io>
2020-04-26 18:08:39 +0200

    test origins

--- api/cors.go
+++ api/cors.go
@@ -7,6 +7,7 @@ import (
 
 func (api *Api) cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
+	log.Fatalf("%+v", api.getOrigins())
 	config.AllowOrigins = api.getOrigins()
 	config.AllowCredentials = true
 	config.AddAllowHeaders("TEAM")
