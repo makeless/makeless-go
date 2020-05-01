f666709db15cf7df68e92b6cd8b2d7a5b7a19fdb
Lucas Löffel <lucas@loeffel.io>
2020-05-01 16:49:58 +0200

    update handler

--- api/event.go
+++ api/event.go
@@ -1,6 +1,7 @@
 package saas_api
 
 import (
+	"fmt"
 	"github.com/gin-contrib/sse"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -24,26 +25,24 @@ func (api *Api) events(c *gin.Context) {
 	clientId := api.GetEvent().NewClientId()
 	api.GetEvent().Subscribe(userId, clientId)
 
-	if err := sse.Encode(w, sse.Event{
-		Data: "test",
-	}); err != nil {
-		api.GetLogger().Println(err.Error())
-	}
-	w.Flush()
+	go func() {
+		api.GetEvent().Emit(userId, sse.Event{
+			Data: fmt.Sprintf("subscribed: %d", clientId),
+		})
+	}()
 
 	for {
 		select {
-		case event, ok := <-api.GetEvent().Listen(userId, clientId):
-			if !ok {
-				continue
-			}
-
+		case event := <-api.GetEvent().Listen(userId, clientId):
 			if err := sse.Encode(w, event); err != nil {
 				api.GetLogger().Println(err.Error())
 			}
 			w.Flush()
 		case <-w.CloseNotify():
 			api.GetEvent().Unsubscribe(userId, clientId)
+			api.GetEvent().Emit(userId, sse.Event{
+				Data: fmt.Sprintf("unsubscribed: %d", clientId),
+			})
 			return
 		}
 	}
