f7c6a6d6c892face72c6276fd98f6e02c0cf4c63
Lucas Löffel <lucas@loeffel.io>
2020-06-06 13:36:53 +0200

    add refresh token

--- saas.go
+++ saas.go
@@ -70,6 +70,7 @@ func (saas *Saas) Init(path string) error {
 	saas.SetRoute("register", saas.register)
 	saas.SetRoute("login", saas.login)
 	saas.SetRoute("logout", saas.logout)
+	saas.SetRoute("refresh-token", saas.refreshToken)
 	saas.SetRoute("user", saas.user)
 
 	return nil
