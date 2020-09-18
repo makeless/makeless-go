cf37b740bb573cb4d3ef47383168cbfde098561a
Lucas Löffel <lucas@loeffel.io>
2020-09-18 15:43:14 +0200

    update

--- team.go
+++ team.go
@@ -135,32 +135,6 @@ func (saas *Saas) createTeam(http go_saas_http.Http) error {
 	return nil
 }
 
-func (saas *Saas) invitationTeam(http go_saas_http.Http) error {
-	http.GetRouter().GET(
-		"/api/auth/team/invitation",
-		http.GetAuthenticator().GetMiddleware().MiddlewareFunc(),
-		func(c *gin.Context) {
-			userId := http.GetAuthenticator().GetAuthUserId(c)
-
-			var err error
-			var tokens []*go_saas_model.Token
-			var user = &go_saas_model.User{
-				Model:   go_saas_model.Model{Id: userId},
-				RWMutex: new(sync.RWMutex),
-			}
-
-			if tokens, err = http.GetDatabase().GetTokens(http.GetDatabase().GetConnection(), user, tokens); err != nil {
-				c.AbortWithStatusJSON(h.StatusInternalServerError, http.Response(err, nil))
-				return
-			}
-
-			c.JSON(h.StatusOK, http.Response(nil, tokens))
-		},
-	)
-
-	return nil
-}
-
 func (saas *Saas) leaveDeleteTeam(http go_saas_http.Http) error {
 	http.GetRouter().DELETE(
 		"/api/auth/team",
