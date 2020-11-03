5bbe9ddfe2cbdfdd633bcf7af59733b1aa60fd62
Lucas Löffel <lucas@loeffel.io>
2020-11-03 15:15:14 +0100

    remove debug

--- database/basic/team.go
+++ database/basic/team.go
@@ -94,7 +94,6 @@ func (database *Database) DeleteTeamUser(connection *gorm.DB, teamUser *makeless
 // DeleteTeam deletes team and all their teamUsers and teamInvitations
 func (database *Database) DeleteTeam(connection *gorm.DB, team *makeless_go_model.Team) error {
 	return connection.
-		Debug().
 		Select("TeamUsers", "TeamInvitations", "Tokens").
 		Delete(team).
 		Error
