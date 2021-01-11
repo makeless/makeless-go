6805043087deae6c63805181b99dde0e90f64de6
Lucas Löffel <lucas@loeffel.io>
2021-01-11 17:37:10 +0100

    extend mailer interface

--- mailer/mailer.go
+++ mailer/mailer.go
@@ -2,6 +2,7 @@ package makeless_go_mailer
 
 import (
 	"context"
+	"github.com/makeless/makeless-go/queue"
 )
 
 type Mailer interface {
@@ -10,6 +11,7 @@ type Mailer interface {
 	GetHandler(name string) (func(data map[string]interface{}) (Mail, error), error)
 	SetHandler(name string, handler func(data map[string]interface{}) (Mail, error))
 	GetMail(name string, data map[string]interface{}) (Mail, error)
+	GetQueue() makeless_go_queue.Queue
 	Send(ctx context.Context, mail Mail) error
 	SendQueue(mail Mail) error
 }
