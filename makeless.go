e856bd42d2628edff255d0f1195f21eb837a13dd
Lucas Löffel <lucas@loeffel.io>
2021-10-18 17:21:58 +0200

    export mails

--- makeless.go
+++ makeless.go
@@ -140,8 +140,8 @@ func (makeless *Makeless) Init(dialector gorm.Dialector, path string) error {
 		}
 	}
 
-	makeless.SetMail("emailVerification", makeless.mailEmailVerification)
-	makeless.SetMail("passwordRequest", makeless.mailPasswordRequest)
+	makeless.SetMail("emailVerification", makeless.MailEmailVerification)
+	makeless.SetMail("passwordRequest", makeless.MailPasswordRequest)
 
 	if makeless.GetConfig().GetConfiguration().GetTeams() != nil {
 		makeless.SetMail("teamInvitation", makeless.mailTeamInvitation)
