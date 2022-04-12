a13daca35e7be74703c4ffd845689dfb67d69d54
Lucas Löffel <lucas@loeffel.io>
2022-04-12 15:17:48 +0200

    update

--- database/database/basic/migrate.go
+++ database/database/basic/migrate.go
@@ -1,5 +1,5 @@
 package makeless_go_database_basic
 
 func (database *Database) Migrate(dst ...interface{}) error {
-	return database.GetConnection().AutoMigrate(dst)
+	return database.GetConnection().AutoMigrate(dst...)
 }
